9d18081db4322200f56c95a3dc7da76037eb59ac
nikhilnigamnik <nikhilnigamnik@gmail.com>
2023-09-01 01:57:52 +0530

    fixed undefined

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -11,34 +11,35 @@ import axios from "axios";
 const UserProfile = () => {
   const user = useSelector((state) => state.auth.user);
   const userBlogs = useSelector((state) => state.userBlog);
-  console.log(userBlogs);
-
   const dispatch = useDispatch();
+
   const logOutUser = () => {
     dispatch(logout());
   };
+
+  useEffect(() => {
+    if (user) {
+      const url = `${userBlogUrl}/${user._id}`;
+      axios
+        .get(url)
+        .then((response) => {
+          dispatch(setUserBlog(response?.data?.userBlog?.blogs));
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, [user]);
 
   if (!user) {
     return <ErrorProfile />;
   }
 
-  useEffect(() => {
-    const Url = `${userBlogUrl}/${user._id}`;
-    axios
-      .get(Url)
-      .then((response) => {
-        dispatch(setUserBlog(response?.data?.userBlog?.blogs));
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
-
   return (
     <section className="flex flex-col justify-center border rounded-xl p-4">
-      <p>Name : {user.name}</p>
-      <p>Email : {user.email}</p>
-      <p>User Name : {user.username}</p>
+      <p>Name: {user.name}</p>
+      <p>Email: {user.email}</p>
+      <p>User Name: {user.username}</p>
       <p>Reset Password</p>
       <div>Your Blogs</div>
       <Button className={"max-w-max"} onClick={logOutUser}>
@@ -47,7 +48,7 @@ const UserProfile = () => {
       <div className="border p-10">
         {userBlogs.map((blog, index) => (
           <div key={index}>
-            <img src={blog.image} />
+            <img src={blog.image} alt={`Blog ${index}`} />
             <p>{blog.title}</p>
             <p>{blog.description}</p>
             <p>{blog.createdAt}</p>
